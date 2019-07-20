8f473059c23d3f6c943f313c3a4d49b281adbcc5
Mujtaba Al-Tameemi <mujtaba.altameemi@gmail.com>
2019-07-20 13:57:21 +0300

    change API

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const url = 'https://randomuser.me/api/';
+const baseUrl = 'https://reqres.in/api';
 
-fetch(url).then(function(response) {
+fetch(`${baseUrl}/users/2`).then(function(response) {
   return response.json();
 }).then(function(json) {
-  const user = json.results[0];
+  const user = json.data;
   const userFirstNameH1 = document.getElementById('user-first-name')
 
-  userFirstNameH1.innerText = `${user.name.first} ${user.name.last}`;
+  userFirstNameH1.innerText = `${user.first_name} ${user.last_name}`;
 })
 
 document.addEventListener('DOMContentLoaded', function() {
